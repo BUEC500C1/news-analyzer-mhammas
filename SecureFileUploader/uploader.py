193053f83a36b794399e9934118ecc0cf967993d
Hammas <hammassaeed47@gmail.com>
2021-02-17 20:05:59 -0500

    update file

--- SecureFileUploader/uploader.py
+++ SecureFileUploader/uploader.py
@@ -1,1 +1,23 @@
 
+def create(File Filename(s)):
+  CreateEvent(UPLOAD_EVENT, timestamp)
+  if(filename == NULL){
+    log(info, "No Filename Provided")
+    SendError
+  }
+  log(info, "File OK")
+  record.filename = Filename
+  record.id = '/file/12345'
+  record.URL = 'hardcoded_URL.com'
+  record.text = 'EXAMPLE TEXT'
+  
+  return success
+
+def delete(record):
+  CreateEvent(DELETE_EVENT, timestamp)
+  log(info, "Delete Part of The File")
+  #Delete part of file
+
+
+def update(text):
+  record.text = text
