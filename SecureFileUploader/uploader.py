80b1a44dcb6d572d394fc14db0181a3f22f2a42d
Hammas <hammassaeed47@gmail.com>
2021-02-21 03:34:20 -0500

    Update uploader.py

--- SecureFileUploader/uploader.py
+++ SecureFileUploader/uploader.py
@@ -2,10 +2,10 @@ def create(filename):
   #CreateEvent(UPLOAD_EVENT, timestamp)
   if(filename is None):
     return 404
-    #log(info, "No Filename Provided")
+    log(info, "No Filename Provided")
     #SendError
 
-  #log(info, "File OK")
+  log(info, "File OK")
   #record.filename = Filename
   #record.id = '/file/12345'
   #record.URL = 'hardcoded_URL.com'
@@ -15,7 +15,7 @@ def create(filename):
 
 def delete(record):
   #CreateEvent(DELETE_EVENT, timestamp)
-  #log(info, "Delete Part of The File")
+  log(info, "Delete Part of The File")
   #Delete part of file
   return 200
 
