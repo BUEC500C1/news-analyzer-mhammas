edcd860c8db83e59e44beba3ffa5c75fb18b2308
Hammas <hammassaeed47@gmail.com>
2021-03-29 11:01:51 -0400

    add nyt api for ingestor

--- NewsfeedIngestor/ingestor.py
+++ NewsfeedIngestor/ingestor.py
@@ -1,10 +1,19 @@
 import logging
+from pynytimes import NYTAPI
+import os
 
 def search_by_keyword(keyword):
 	if keyword is None:
 		logging.info("No Keyword Specified")
 		return {"status": 404, "message": "No Keyword Specified"}
-
+	api_key = os.getenv("nyt_key")
+	nyt = NYTAPI(str(api_key), parse_dates=True)
 	keyword = str(keyword)
-
-	return {"articles": [1, 2, 3, 4, 5], "status": 200}
+	articles = nyt.article_search(query=keyword,results=10,)
+	ret = []
+	for article in articles:
+		item = dict()
+		item["Summary"] = article.get('snippet')
+		item["URL"] = article.get('web_url')
+		ret.append(item)
+	return {"articles": ret, "status": 200}
